d180c4d8a01745b4c03bd9e0d2ccab6185179692
Anas Yaseen <Anasyaseen1@outlook.com>
2020-12-31 17:30:03 +0200

    1.0 Weather-Compare

--- src/App.js
+++ src/App.js
@@ -1,25 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Compare from "./components/Compare";
 
-function App() {
+import GlobalStyles from "./GlobalStyles";
+const App = () => {
+  const [query, setQuery] = useState("Amman");
+  const [secondQuery, setSecondQuery] = useState("Paris");
+  const [firstCity, setFirstCity] = useState(query);
+  const [secondCity, setSecondCity] = useState(secondQuery);
+  const [isLoading, setIsLoading] = useState(true);
+  const firstCityInfo = `https://api.openweathermap.org/data/2.5/forecast?q=${firstCity}&appid=129caec24c3baf7ececc36c32c164313`;
+  const secondCityInfo = `https://api.openweathermap.org/data/2.5/forecast?q=${secondCity}&appid=129caec24c3baf7ececc36c32c164313`;
+  const [firstCityData, setFirstCityData] = useState("");
+  const [secondCityData, setSecondCityData] = useState("");
+  const firstInputHandler = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const secondInputHandler = (e) => {
+    setSecondQuery(e.target.value);
+  };
+
+  const compareHandler = async (e) => {
+    e.preventDefault();
+    const firstCityData = await fetch(firstCityInfo)
+      .then((res) => res.json())
+      .then((info) => info);
+    const secondCityData = await fetch(secondCityInfo)
+      .then((res) => res.json())
+      .then((info) => info);
+
+    setFirstCityData(firstCityData);
+    setSecondCityData(secondCityData);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    setFirstCity(query);
+    setSecondCity(secondQuery);
+  }, [query, secondQuery]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GlobalStyles />
+      <Container>
+        <Content>
+          <h3>
+            Compare the weather for the following 3 days between any two cities.
+          </h3>
+          <Form>
+            <InputBox onChange={firstInputHandler} value={query} />
+            <InputBox onChange={secondInputHandler} value={secondQuery} />
+
+            <Button onClick={compareHandler}>Compare</Button>
+          </Form>
+          {isLoading ? (
+            <h3 class="alert">Please click compare</h3>
+          ) : (
+            <Compare
+              firstCityData={firstCityData}
+              secondCityData={secondCityData}
+            />
+          )}
+        </Content>
+      </Container>
+      );
     </div>
   );
-}
+};
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 5rem 5rem;
+  height: 100vh;
+  font-family: "Poppins", sans-serif;
+  background: hsl(212, 33%, 89%);
+
+  .alert {
+    margin: 2rem 0;
+    color: #ad9a9a;
+  }
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+
+  h3 {
+    color: #102a43;
+    margin-bottom: 1rem;
+  }
+  p {
+    color: #334e68;
+  }
+
+  .btn {
+    font-weight: bold;
+  }
+`;
+
+const Form = styled.form`
+  margin-top: 2rem;
+`;
+
+const InputBox = styled.input`
+  font-family: inherit;
+  font-size: 16px;
+  padding: 0.75rem 1rem;
+  border: none;
+  border-radius: 1rem;
+  margin-right: 2rem;
+`;
+
+const Button = styled.button`
+  font-family: inherit;
+  font-size: 16px;
+  padding: 0.75rem 1rem;
+  border: none;
+  border-radius: 1rem;
+  background: #044e54;
+  color: hsl(212, 33%, 89%);
+  margin-right: 2rem;
+
+  &:hover {
+    background: #3a8086;
+  }
+  &:active {
+    scale: 0.98;
+  }
+`;
 
 export default App;
---
+++ src/components/Compare.js
@@ -0,0 +1,109 @@
+import React from "react";
+import styled from "styled-components";
+
+const Compare = ({ firstCityData, secondCityData }) => {
+  const filteredFirstCityData = firstCityData.list.filter((item) =>
+    item.dt_txt.includes("12:00:00")
+  );
+  const filteredSecondCityData = secondCityData.list.filter((item) =>
+    item.dt_txt.includes("12:00:00")
+  );
+  const filteredNightFirstCityData = firstCityData.list.filter((item) =>
+    item.dt_txt.includes("06:00:00")
+  );
+  const filteredNightSecondCityData = secondCityData.list.filter((item) =>
+    item.dt_txt.includes("06:00:00")
+  );
+
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th colSpan="2">Name</th>
+          <th colSpan="2">Temp. Day °C</th>
+          <th colSpan="2">Temp. Night °C</th>
+          <th colSpan="2">Conditions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{filteredSecondCityData[1].dt_txt.split(" ")[0]}</td>
+          <td>{firstCityData.city.name}</td>
+          <td>{secondCityData.city.name}</td>
+          <td>{Math.floor(filteredFirstCityData[1].main.temp - 273.15)}</td>
+          <td>{Math.floor(filteredSecondCityData[1].main.temp - 273.15)}</td>
+          <td>
+            {Math.floor(filteredNightFirstCityData[1].main.temp - 273.15)}
+          </td>
+          <td>
+            {Math.floor(filteredNightSecondCityData[1].main.temp - 273.15)}
+          </td>
+          <td>{filteredFirstCityData[1].weather[0].main}</td>
+          <td>{filteredSecondCityData[1].weather[0].main}</td>
+        </tr>
+        <tr>
+          <td>{filteredSecondCityData[2].dt_txt.split(" ")[0]}</td>
+          <td>{firstCityData.city.name}</td>
+          <td>{secondCityData.city.name}</td>
+          <td>{Math.floor(filteredFirstCityData[2].main.temp - 273.15)}</td>
+          <td>{Math.floor(filteredSecondCityData[2].main.temp - 273.15)}</td>
+          <td>
+            {Math.floor(filteredNightFirstCityData[2].main.temp - 273.15)}
+          </td>
+          <td>
+            {Math.floor(filteredNightSecondCityData[2].main.temp - 273.15)}
+          </td>
+          <td>{filteredFirstCityData[2].weather[0].main}</td>
+          <td>{filteredSecondCityData[2].weather[0].main}</td>
+        </tr>
+        <tr>
+          <td>{filteredSecondCityData[3].dt_txt.split(" ")[0]}</td>
+          <td>{firstCityData.city.name}</td>
+          <td>{secondCityData.city.name}</td>
+          <td>{Math.floor(filteredFirstCityData[3].main.temp - 273.15)}</td>
+          <td>{Math.floor(filteredSecondCityData[3].main.temp - 273.15)}</td>
+          <td>
+            {Math.floor(filteredNightFirstCityData[3].main.temp - 273.15)}
+          </td>
+          <td>
+            {Math.floor(filteredNightSecondCityData[3].main.temp - 273.15)}
+          </td>
+          <td>{filteredFirstCityData[3].weather[0].main}</td>
+          <td>{filteredSecondCityData[3].weather[0].main}</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+};
+
+const Table = styled.table`
+  font-family: inherit;
+  width: 100%;
+  margin-top: 2rem;
+  padding: 1rem;
+  border: 2px solid #555353;
+  text-align: center;
+  border-collapse: collapse;
+  height: 300px;
+  tr {
+    border-bottom: 1px solid #ccc;
+  }
+  th,
+  td {
+    text-align: left;
+    padding: 1rem;
+    border-right: 1px solid#ccc;
+    text-transform: capitalize;
+  }
+  td:nth-of-type(even) {
+    background: lightblue;
+  }
+  td:nth-of-type(odd) {
+    background: #0e7c86;
+  }
+  td:first-child {
+    background: #dae2ec;
+  }
+`;
+export default Compare;
